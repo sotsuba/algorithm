3faf0ddc2cc792a5d75146500397f2c833468f68
sotsuba <tienson19273@gmail.com>
2026-01-30 01:15:47 +0700

    daily-sync: 2026-01-30

--- problems/cses/05-range_queries/1647-static_range_minimum_queries/main.rs
+++ problems/cses/05-range_queries/1647-static_range_minimum_queries/main.rs
@@ -5,7 +5,11 @@ struct Scanner<'a> {
 }
 
 impl<'a> Scanner<'a> {
-    fn new(input: &'a str) -> Self {
+    fn new() -> Self {
+        let mut input = String::new();
+        io::stdin()
+            .read_to_string(&mut input)
+            .expect("Cannot read the stdin");
         Self {
             iter: input.split_whitespace(),
         }
@@ -29,65 +33,13 @@ struct Rmq {
 
 const INF: usize = usize::MAX;
 
-impl Rmq {
-    pub fn new(block_size: usize, data: &[usize], n: usize) -> Self {
-        let block_len = n.div_ceil(block_size);
-        let mut block_min = vec![INF; block_len + 1];
-        for i in 1..=n {
-            let id = i / block_size;
-            block_min[id] = block_min[id].min(data[i]);
-        }
-        Self {
-            block_size,
-            block_min,
-            data: data.to_vec(),
-        }
-    }
-
-    pub fn query(&self, left_bound: usize, right_bound: usize) -> usize {
-        let left_block_id = left_bound / self.block_size;
-        let right_block_id = right_bound / self.block_size;
-        let remain_right = right_bound % self.block_size;
-        let remain_left = self.block_size * (left_block_id + 1) - left_bound;
-        let mut ans = INF;
-        for i in (left_block_id + 1)..right_block_id {
-            ans = ans.min(self.block_min[i]);
-        }
-        if left_block_id == right_block_id {
-            for i in left_bound..=right_bound {
-                ans = ans.min(self.data[i]);
-            }
-        } else {
-            for i in (right_bound - remain_right)..=right_bound {
-                ans = ans.min(self.data[i]);
-            }
-            for i in left_bound..(left_bound + remain_left) {
-                ans = ans.min(self.data[i]);
-            }
-        }
-        ans
-    }
-}
-
 fn main() {
-    let mut input = String::new();
-    io::stdin()
-        .read_to_string(&mut input)
-        .expect("Cannot read the stdin");
-
-    let mut sc = Scanner::new(&input);
+    let mut sc = Scanner::new);
     let n: usize = sc.next();
     let q: usize = sc.next();
 
     let mut arr = vec![0_usize; n + 1];
     for i in 1..=n {
         arr[i] = sc.next();
-    }
-    let rmq = Rmq::new(1000, &arr, n);
-    for _ in 0..q {
-        let l: usize = sc.next();
-        let r: usize = sc.next();
-
-        println!("{}", rmq.query(l, r));
     }
 }
